880db99d0a54553f4f6288bd832796482c1f0147
tcolak <tcolak@localhost.localdomain>
2016-12-14 16:20:07 +0300

    plugin version 1.0.0-alfa

--- ahenk-local-user/local-user/main.py
+++ ahenk-local-user/local-user/main.py
@@ -5,7 +5,7 @@
 def info():
     inf = dict()
     inf['name'] = 'local-user'
-    inf['version'] = '1.0.0'
+    inf['version'] = '1.0.0-alfa'
     inf['support'] = 'debian'
     inf['description'] = 'Local-User plugin provides to listing users and adding, editing, deleting a local user.'
     inf['task'] = True
--- lider-console-local-user/src/tr/org/liderahenk/localuser/constants/LocalUserConstants.java
+++ lider-console-local-user/src/tr/org/liderahenk/localuser/constants/LocalUserConstants.java
@@ -4,7 +4,7 @@ public class LocalUserConstants {
 	
 	public static final String PLUGIN_NAME = "local-user";
 	
-	public static final String PLUGIN_VERSION = "1.0.0";
+	public static final String PLUGIN_VERSION = "1.0.0-alfa";
 	
 	public static final class PARAMETERS {
 		public static final String USERNAME = "username";
