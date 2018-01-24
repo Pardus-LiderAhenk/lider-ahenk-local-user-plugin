0bc7a95189c29249d78e59233e4178429c7df82f
Tuncay ÇOLAK <tuncay.colak@tubitak.gov.tr>
2018-01-24 16:42:49 +0300

    version name fix: 1.1

--- lider-console-local-user/src/tr/org/liderahenk/localuser/constants/LocalUserConstants.java
+++ lider-console-local-user/src/tr/org/liderahenk/localuser/constants/LocalUserConstants.java
@@ -4,7 +4,7 @@ public class LocalUserConstants {
 	
 	public static final String PLUGIN_NAME = "local-user";
 	
-	public static final String PLUGIN_VERSION = "1.0.0";
+	public static final String PLUGIN_VERSION = "1.1";
 	
 	public static final class PARAMETERS {
 		public static final String USERNAME = "username";
