3b76814ab324944a571c9358759390a3ab93c30c
hasankara <h.kara27@gmail.com>
2018-09-20 14:12:30 +0300

    xfce4 conf is added

--- ahenk-local-user/local-user/add_user.py
+++ ahenk-local-user/local-user/add_user.py
@@ -89,17 +89,6 @@ class AddUser(AbstractPlugin):
             #
             # Handle kiosk mode
             #
-            # if xfce4-panel.xml does not exist copy it from ~/.config/xfce4/xfconf/xfce-perchannel-xml/
-            file_xfce4_panel = Path("/etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml")
-            if not file_xfce4_panel.exists():
-                self.logger.error(
-                    'PANEL XML NOT FOUND COPY')
-                source_path = "{0}local-user/panelconf/xfce4-panel.xml".format(self.Ahenk.plugins_path())
-                self.logger.info("----->>>>" + source_path)
-                self.copy_file(source_path, "/etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml")
-                self.logger.error(
-                    'FILE IS COPIED')
-
             result_code, p_out, p_err = self.execute(self.script.format('find_locked_users.sh'), result=True)
             if result_code != 0:
                 self.logger.error(
